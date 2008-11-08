ea47c2ae0314b2f7a19f84e5180ba88492f1e7b1
Victor Goff <keeperotphones@gmail.com>
2008-11-08 14:02:19 -0600

    Added first doctest and samples directory.

---
+++ samples/lib/first.rb
@@ -0,0 +1,9 @@
+#!/bin/evn ruby
+
+# This is a program to simply test assumptions about assignment and precedence.
+# Use first.doctest to ensure nothing has changed in any methods created here.
+
+def hello
+  return 'Hello World!'
+end
+
