73c813785d3065243459d65c37aa0e8726515414
Victor Goff <keeperotphones@gmail.com>
2008-11-09 16:32:12 -0600

    considering how to handle "puts" in rubydoctest

--- samples/lib/first.rb
+++ samples/lib/first.rb
@@ -1,4 +1,4 @@
-#!/bin/evn ruby
+#!/bin/env ruby
 
 # This is a program to simply test assumptions about assignment and precedence.
 # Use first.doctest to ensure nothing has changed in any methods created here.
