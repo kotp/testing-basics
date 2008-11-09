40502335a692d24b26a45afe57eddba44966b7ea
Victor Goff <keeperotphones@gmail.com>
2008-11-09 13:28:31 -0600

    Added toplevel outline

--- samples/lib/first.rb
+++ samples/lib/first.rb
@@ -1,6 +1,5 @@
-#!/bin/evn ruby
+#!/bin/env ruby
 
-# This is a program to simply test assumptions about assignment and precedence.
 # Use first.doctest to ensure nothing has changed in any methods created here.
 
 def hello
